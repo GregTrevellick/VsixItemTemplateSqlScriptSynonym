9ab7a308f7ecea961f047bb7169c47a0c536728d
Greg Trevellick <gtrevellick@googlemail.com>
2020-05-18 20:11:01 +0100

    ..

--- Src/ItemTemplate/ItemTemplate.sql
+++ Src/ItemTemplate/ItemTemplate.sql
@@ -1,4 +1,6 @@
-﻿IF EXISTS 
+﻿/* Changes to this template can be proposed at https://github.com/GregTrevellick/VsixItemTemplateSqlScriptSynonym/blob/master/Src/ItemTemplate/ItemTemplate.sql */
+
+IF EXISTS 
         (SELECT 1 
         FROM sys.synonyms syn
         INNER JOIN sys.schemas sch ON sch.schema_id = syn.schema_id
